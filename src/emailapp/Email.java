394d75f5ed66c7868d15359c9837fbdd743b3ced
lissette soto <lissette.soto156@gmail.com>
2022-12-12 21:04:21 -0800

    first commit

---
+++ src/emailapp/Email.java
@@ -0,0 +1,26 @@
+package emailapp;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String departmentName;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+
+    //Constructor to recieve the first and last name
+    public Email (String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email Created: " + this.firstName + " "+  this.lastName);
+    }
+
+    //Ask for the department
+
+    //Generate a random password
+
+    //set the alternate email
+
+    //Change the password
+}
